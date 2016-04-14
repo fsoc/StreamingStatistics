ffbfa6c5cef92d7d69602e908da9a0b3adf21faf
fsoc <fsoc@null>
2016-04-14 16:14:24 +0200

    Keeping track of max low also for subtrees.

--- src/test/java/fsoc/ITreeTest.java
+++ src/test/java/fsoc/ITreeTest.java
@@ -14,13 +14,14 @@ public class ITreeTest {
     tree.add(3, 41, 1);
     tree.add(0, 1, 1);
     tree.add(10, 15, 1);
+    tree.add(7, 8, 1);
     return tree;
   }
 
   @Test
   public void testInsert() {
     ITree tree = createTree();
-    String ans = "(20,36) 99 #(3,41) 41 (29,99) 99 #(0,1) 1 (10,15) 15 ####";
+    String ans = "(20,36) 0;99 #(3,41) 0;41 (29,99) 29;99 #(0,1) 0;1 (10,15) 7;15 ###(7,8) 7;8 ##";
     assertEquals(ans, ""+tree);
   }
 
@@ -42,9 +43,9 @@ public class ITreeTest {
   public void testOneIntersection() {
     ITree tree = createTree();
     LinkedList<Node> intersections = tree.getIntersections(98, 100);
-    String strAns = "" + intersections.get(0);
-    String correctAns = "(29,99) 99 ";
     assertEquals(1, intersections.size());
+    String strAns = "" + intersections.get(0);
+    String correctAns = "(29,99) 29;99 ";
     assertEquals(correctAns, strAns);
   }
 
@@ -52,9 +53,9 @@ public class ITreeTest {
   public void testOneIntersectionSmall() {
     ITree tree = createTree();
     LinkedList<Node> intersections = tree.getIntersections(0, 1);
-    String strAns = "" + intersections.get(0);
-    String correctAns = "(0,1) 1 ";
     assertEquals(1, intersections.size());
+    String strAns = "" + intersections.get(0);
+    String correctAns = "(0,1) 0;1 ";
     assertEquals(correctAns, strAns);
   }
 
