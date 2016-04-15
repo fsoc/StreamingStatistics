1d26f54ba1e96063f96243c97cb55dbd1dc6e7fa
fsoc <fsoc@altai.se>
2016-04-15 08:43:04 +0200

    More loadtests.

--- samples/Generator.java
+++ samples/Generator.java
@@ -3,6 +3,53 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Generator  {
   public static void main(String[] args) {
+    print3();
+  }
+
+  private static void print3() {
+    int num = 100000;
+    long min = 1325000000000L;
+    long maxd = 30000000L;
+
+    System.out.println(num);
+
+    for ( int i = 0; i< num; i++) {
+      System.out.println(min+" "+maxd+" "+64);
+    }
+
+    System.out.println(num);
+
+    for ( int i = 0; i< num; i++) {
+      System.out.println(min+" "+(min + (maxd-1)));
+    }
+  }
+
+  private static void print2() {
+    int num = 100000;
+    long min = 1325000000000L;
+
+    System.out.println(num);
+
+    long tempMin = min;
+    for ( int i = 0; i< num; i++) {
+      long a = tempMin;
+      long b = 3;
+      tempMin += 4;
+      System.out.println(a+" "+b+" "+64);
+    }
+
+    System.out.println(num);
+
+    tempMin = min;
+    for ( int i = 0; i< num; i++) {
+      long a = tempMin + 1;
+      long b = tempMin + 2;
+      tempMin += 4;
+      System.out.println(a+" "+b);
+    }
+  }
+
+  private static void print1() {
     int num = 100000;
     long min = 1325000000000L;
     long max = 1326000000000L;
